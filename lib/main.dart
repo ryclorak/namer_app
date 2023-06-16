e3538359c9335f4188046845d6b9cef63c408be2
ryclorak <ryclorak@gmail.com>
2023-06-16 10:42:53 -0700

    Refactor, adding widget for word pair card

--- lib/main.dart
+++ lib/main.dart
@@ -38,12 +38,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:\n\n'),
-          Text(appState.current.asLowerCase),
+          Text('A random word pair:\n\n'),
+          BigCard(pair: pair),
 
           ElevatedButton(
             onPressed: () {
@@ -52,6 +53,31 @@ class MyHomePage extends StatelessWidget {
             child: Text('Next'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
